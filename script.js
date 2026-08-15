9824e0e182660041f3ec62cc22a879072190df77
maarifkabpasuruan <maarifkabpasuruan@gmail.com>
2026-08-15 14:07:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Ma'arif website aktif");
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark") ? "dark" : "light"
+  );
+});
+
+if (localStorage.getItem("theme") === "dark") {
+  document.body.classList.add("dark");
+}
